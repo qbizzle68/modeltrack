9d106cc36331ded85361ff0f5ebf9a2cfb1a650c
qbarnes <quinton.barnes68@gmail.com>
2023-07-19 13:51:17 -0500

    created setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='model_track',
+    version='0.0.1.0',
+    author='Quinton Barnes',
+    description='A package for tracking diecast model progress.',
+    packages=['model_track'],
+    package_dir={'': 'src'}
+)
