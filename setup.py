d2091f433a21779923db230aae7f5d4d9af32436
qbarnes <quinton.barnes68@gmail.com>
2023-07-19 13:53:11 -0500

    removed hyphen from name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='model_track',
+    name='modeltrack',
     version='0.0.1.0',
     author='Quinton Barnes',
     description='A package for tracking diecast model progress.',
-    packages=['model_track'],
+    packages=['model-track'],
     package_dir={'': 'src'}
 )
