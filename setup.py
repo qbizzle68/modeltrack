198d826ce60d5db17d125dac6e51f7bb603e1936
qbarnes <quinton.barnes68@gmail.com>
2023-07-23 23:10:50 -0500

    "version 0.1.0"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='modeltrack',
-    version='0.0.1.0',
+    version='0.1.0',
     author='Quinton Barnes',
     description='A package for tracking diecast model progress.',
     packages=['modeltrack'],
